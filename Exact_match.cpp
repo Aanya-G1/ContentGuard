c8dce9f99d58b62c9f04ec11bc29477c74198b66
Drishti215 <1029drishti@gmail.com>
2026-05-28 13:06:55 +0530

    Update Exact Match backend module with corrected source code

--- Exact_match.cpp
+++ Exact_match.cpp
@@ -1,62 +1,44 @@
+#include "httplib.h"
+#include <nlohmann/json.hpp>
+#include <string>
 #include <iostream>
-#include "Exact_Match.h"
-using namespace std;
-void computeLPS(string pat, vector<int> &lps) {
-    int len = 0;
-    lps[0] = 0;
-    int i = 1;
-    while (i < pat.size()) {
-        if (pat[i] == pat[len]) {
-            len++;
-            lps[i] = len;  
-            i++;
-        } else {
-            if (len != 0) {
-                len = lps[len - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
-        }
-    }
+
+// Use nlohmann::json
+using json = nlohmann::json;
+
+// Simple KMP-like search (for demo, using std::string::find)
+bool kmpSearch(const std::string &text, const std::string &pattern) {
+    return text.find(pattern) != std::string::npos;
 }
-void kmpSearchAndInsert(string doc1, string doc2, string pattern, PGconn *conn) {
-    vector<int> lps(pattern.size());
-    computeLPS(pattern, lps);
-    int i = 0, j = 0;
-    while (i < doc1.size()) {
-        if (doc1[i] == pattern[j]) {
-            i++;
-            j++;
-        }
-        if (j == pattern.size()) {
-            int position = i - j;
-            cout << "Match found at position: " << position << endl;
-            int matchLength = pattern.size();
-            float similarity = (float)matchLength / doc1.size();
-            //insert query
-            string query =
-                "INSERT INTO matches (doc1, doc2, match_text, position, match_length, similarity, created_at) VALUES ('" +
-                doc1 + "','" +
-                doc2 + "','" +
-                pattern + "'," +
-                to_string(position) + "," +
-                to_string(matchLength) + "," +
-                to_string(similarity) + ", NOW());";
-            PGresult *res = PQexec(conn, query.c_str());
-            if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-                cout << " Insert failed: " << PQerrorMessage(conn) << endl;
-            } else {
-                cout << " Data inserted into DB!\n";
-            }
-            PQclear(res);
-            j = lps[j - 1];
-        }
-        else if (i < doc1.size() && doc1[i] != pattern[j]) {
-            if (j != 0)
-                j = lps[j - 1];
-            else
-                i++;
-        }
-    }
+
+int main() {
+    httplib::Server svr;
+
+    svr.Post("/check", [&](const httplib::Request &req, httplib::Response &res) {
+        // Parse JSON request
+        json jsonReq = json::parse(req.body);
+        std::string text = jsonReq["text"];
+        std::string pattern = jsonReq["pattern"];
+
+        // Run search
+        bool found = kmpSearch(text, pattern);
+        std::string result = found ? "Pattern found!" : "Pattern not found.";
+
+        // Build JSON response
+        json response;
+        response["result"] = result;
+        response["found"] = found;
+
+        // Dummy matches list (since DB not connected yet)
+        response["matches"] = {
+            { {"id", 1}, {"text", "example one"} },
+            { {"id", 2}, {"text", "example two"} }
+        };
+
+        // Send response
+        res.set_content(response.dump(), "application/json");
+    });
+
+    std::cout << "Server running at http://localhost:5000\n";
+    svr.listen("0.0.0.0", 5000);
 }
